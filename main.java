fb9c6404f85ae764a6a475642d5b20d49602ce92
MKY <2410030074@klh.edu.in>
2026-03-09 11:37:46 +0530

    Resolve merge conflict

--- main.java
+++ main.java
@@ -1,5 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Git Lab Program");
+        System.out.println("Feature version");
+        System.out.println("Bug fix version");
     }
 }
